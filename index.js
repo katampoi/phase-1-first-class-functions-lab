d2b835041e53bcfc8ecddd2e8f4a995a7af7ae71
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-10 13:01:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+const newDrivers = [...drivers];
+return newDrivers.splice(0,2)
+};
+
+const returnLastTwoDrivers = (drivers) => {
+    const newDrivers = [...drivers];
+    return newDrivers.splice(-2)
+    };
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (val) => {
+    return function(fare){
+        return fare * val;
+}       
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, FirstTwoDrivers) =>{
+    return FirstTwoDrivers(drivers)
+}
+    
